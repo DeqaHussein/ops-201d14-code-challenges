48d40dcbfe9eb88f9d3cb8dcc9af368cc3371c7f
Deqa Hussein <Deqahussein1220@gmail.com>
2023-10-24 19:34:17 -0500

    hello world

---
+++ helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script Name:                HelloWorld.sh
+# Author:                       DeqaHussein
+# Date of latest revision:      10/23/2023
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+echo "Hello World"
+echo "I am working perfectly"
+# End
