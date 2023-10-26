51007ff75a775699ab8b62e7951858b70f0ba9f2
Deqa Hussein <Deqahussein1220@gmail.com>
2023-10-25 20:54:20 -0500

    done with function code

---
+++ Function.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Script Name:                Function
+# Author:                       DeqaHussein
+# Date of latest revision:      10/25/2023
+# Purpose:                      bash function.sh
+
+# Declaration of variables
+Login="This is the login history"
+
+# Declaration of functions
+
+
+#
+print_something () {
+  last
+  echo $Login
+   
+}
+
+# Main
+print_something
+print_something
+print_something
+
+
+
+# End
